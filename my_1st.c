df6da73a44593b4e045e84a4d3c400af4e97821b
SHIVAM <shivamverma3828@gmail.com>
2024-07-20 17:19:21 +0530

    1st file feature pushed to SVDEV1_feature

--- my_1st.c
+++ my_1st.c
@@ -4,4 +4,6 @@ void main()
     printf("1st file pushed to git");
     printf("1st file push in SVDEV1");
     printf("2nd filr push in SVDEV1");
+    printf("1st file push in SVDEV1/FEATURE1");
+
 }
